2ee72d686b5769bcec5f61e61d447fa2df05ad41
Matteo Kinkela <matteo.kinkela@flipkod.com>
2020-04-10 10:21:13 +0200

    default null, not false

--- api/models/warningScore.js
+++ api/models/warningScore.js
@@ -6,11 +6,11 @@ const warningScoreSchema = mongoose.Schema({
     numberOfRespirations: { type: Number, required: true },
     oxygenSaturation: { type: Number, required: true },
     anyAdditionalO2: { type: Boolean, required: true },
+    onRespirator: { type: Boolean, required: true },
     systolicPressure: { type: Number, required: true },
     heartRate: { type: Number, required: true },
     stateOfConsciousness: { type: String, required: true },
     bodyTemperature: { type: Number, required: true },
-    coughDegree: { type: Number, min: 1, max: 5, required: false },
     score: { type: Number, required: true },
     timestamp: { type: Date, default: Date.now }
 });
