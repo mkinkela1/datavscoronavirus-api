7106d41dd77ed33f2529e97605de8beec1c8e449
Matteo Kinkela <matteo.kinkela@flipkod.com>
2020-04-10 10:22:33 +0200

    Create PatientRelevantDataController.js

---
+++ api/controllers/PatientRelevantDataController.js
@@ -0,0 +1,95 @@
+const PatientRelevantData = require('../models/patientRelevantData');
+const Patient = require('../models/patient');
+const Mongoose = require('mongoose');
+
+/**
+ * Create patient's relevant data
+ *
+ * @param req
+ * @param res
+ * @param next
+ */
+exports.createPatientRelevantData = (req, res, next) => {
+
+    const { patientId } = req.params;
+    const { body } = req;
+
+    const relevantData = new PatientRelevantData(body);
+
+    relevantData.save(e => {
+
+        if(e) res.status(500).json(e);
+
+        Patient
+            .findOneAndUpdate(
+                { _id: patientId },
+                { $push: { patientRelevantData: relevantData } },
+                { returnOriginal: false, new: true, upsert: true }
+            )
+            .populate('PatientRelevantData')
+            .exec()
+            .then(r => res.status(201).json(r))
+            .catch(e => res.status(500).json(e));
+    });
+};
+
+/**
+ * Get all patient's relevant data
+ *
+ * @param req
+ * @param res
+ * @param next
+ */
+exports.getAllPatientRelevantData = (req, res, next) => {
+
+    const { patientId } = req.params;
+
+    Patient
+        .find({ _id: patientId })
+        .populate('PatientRelevantData')
+        .exec()
+        .then(r => res.status(200).json(r))
+        .catch(e => res.status(500).json(e));
+};
+
+/**
+ * Get single relevant patient data by id
+ *
+ * @param req
+ * @param res
+ * @param next
+ */
+exports.getPatientRelevantDataById = (req, res, next) => {
+
+    const { patientRelevantDataId } = req.params;
+
+    PatientRelevantData
+        .findOne({ _id: patientRelevantDataId })
+        .exec()
+        .then(r => res.status(200).json(r))
+        .catch(e => res.status(500).json(e));
+};
+
+/**
+ * Update patient relevant data
+ *
+ * @param req
+ * @param res
+ * @param next
+ */
+exports.updatePatientRelevantData = (req, res, next) => {
+
+    const { patientRelevantDataId } = req.params;
+    const { body } = req;
+
+    PatientRelevantData
+        .findOneAndUpdate(
+            { _id: patientRelevantDataId },
+            body,
+            { returnOriginal: false, new: true, upsert: true }
+
+        )
+        .exec()
+        .then(r => res.status(200).json(r))
+        .catch(e => res.status(500).json(e));
+};
